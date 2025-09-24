e85bb59ed93aa721bb42fa0d7186ecb879337a22
Git-Monke <javelasq2006@gmail.com>
2025-09-24 09:03:31 -0700

    Switched to vim.lsp.config

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -2,25 +2,18 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = { "williamboman/mason-lspconfig.nvim", "saghen/blink.cmp" },
   config = function()
-    local lspconfig = require("lspconfig")
-    local util = require("lspconfig.util")
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      single_file_support = true,
-      root_dir = util.root_pattern(".git"),
-      settings = {
-        Lua = {
-          diagnostics = { globals = { "vim" } },
-          workspace = {
-            checkThirdParty = false,
-            library = vim.api.nvim_get_runtime_file("", true), -- <-- this line,
-          },
-        },
-      },
+    vim.lsp.config('*', {
+      root_markers = { '.git' },
     })
 
-    lspconfig.astro.setup({})
+    vim.lsp.config('*', {
+      capabilities = {
+        textDocument = {
+          semanticTokens = {
+            multilineTokenSupport = true,
+          }
+        }
+      }
+    })
   end,
 }
